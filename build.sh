8b9ae34174a5c3642dafee2d2bef341b75a3a749
Mridula <66699525+mpeddada1@users.noreply.github.com>
2023-08-10 10:38:44 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-function repo_root() {
-  local dir="$1"
-  while [[ ! -d "${dir}/.git" ]]; do
-    dir="$(dirname "$dir")"
-  done
-  echo "${dir}"
-}
-
 script_home=$(mktemp -d)
 
 git clone https://github.com/mpeddada1/layer1_upstream "$script_home"/layer1_upstream
-# Executor script home
+
+# Path to executor script
 ls $script_home
-
-
-SCRIPT_PATH="$(realpath "$0")"
-SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
-PROJECT_ROOT="$(repo_root "${SCRIPT_DIR}")"
 
 bash "${script_home}"/layer1_upstream/executor.sh run_graalvm_tests.sh
